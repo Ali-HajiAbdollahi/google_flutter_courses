5e5716c31c3892894adddf53ab0ba4362171cac0
Ali Hajiabdollahi <ali.hajiabdollahi.ce@gmail.com>
2026-02-18 05:29:03 +0100

    finished course

---
+++ animated_layout/lib/models/widgets/reply_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:animated_layout/models/data.dart' as data;
+import 'package:animated_layout/models/widgets/email_widget.dart';
+import 'package:flutter/material.dart';
+
+
+class ReplyListView extends StatelessWidget {
+  const ReplyListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: ListView(
+        children: [
+          const SizedBox(height: 8),
+          ...List.generate(data.replies.length, (index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: EmailWidget(
+                email: data.replies[index],
+                isPreview: false,
+                isThreaded: true,
+                showHeadline: index == 0,
+              ),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+}
